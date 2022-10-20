0af8e5cfe879006df04466757039cf3339cb51ab
spyrkola <sp.kolagis@gmail.com>
2022-10-20 22:33:01 +0300

    Initial commit of summing two vectors

---
+++ src/main.cpp
@@ -0,0 +1,28 @@
+// main.cpp
+#include <iostream>
+#include <vector>
+
+/// return a vector that is the element-wise addition of left and right
+std::vector<int> sum_vecs(std::vector<int> const &left, std::vector<int> const &right) {
+	int size = left.size();
+	std::vector<int> result(size);
+
+	for (int i = 0; i < size; i++) {
+		result[i] = left[i] + right[i];
+	}
+	return result;
+}
+
+int main() {
+	std::vector<int> vec1{1, 2, 3, 4, 5};
+	std::vector<int> vec2{6, 7, 8, 9, 10};
+	std::vector<int> vec3 = sum_vecs(vec1, vec2);
+
+	// print the resulting vector
+	int size = vec3.size();
+	for (int i = 0; i < size; i++) {
+		std::cout << vec3[i] << " ";
+	}
+	std::cout << "\n";
+	return 0;
+}
