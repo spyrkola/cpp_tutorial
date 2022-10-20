4f26ad51895a4b58be4b312011c9fa33161530a5
spyrkola <sp.kolagis@gmail.com>
2022-10-21 01:45:43 +0300

    Refactor our library to a template, header-only one

--- include/vector_math.h
+++ include/vector_math.h
@@ -1,8 +1,25 @@
 /// vector_math.h
 #include <vector>
+#include <iostream>
 
 /// return a vector that is the element-wise addition of left and right
-std::vector<int> sum_vecs(std::vector<int> const &left, std::vector<int> const &right);
+template<typename T>
+std::vector<T> sum_vecs(std::vector<T> const &left, std::vector<T> const &right) {
+	int size = left.size();
+	std::vector<T> result(size);
+
+	for (int i = 0; i < size; i++) {
+		result[i] = left[i] + right[i];
+	}
+	return result;
+}
 
 /// print a vector element by element in one line
-void print_vec(std::vector<int> const &vec);
+template<typename T>
+void print_vec(std::vector<T> const &vec) {
+	int size = vec.size();
+	for (int i = 0; i < size; i++) {
+		std::cout << vec[i] << " ";
+	}
+	std::cout << "\n";
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,14 @@
 // main.cpp
-#include <iostream>
 #include <vector>
-
 #include "vector_math.h"
 
 int main() {
 	std::vector<int> vec1{1, 2, 3, 4, 5};
 	std::vector<int> vec2{6, 7, 8, 9, 10};
-	std::vector<int> vec3 = sum_vecs(vec1, vec2);
+	std::vector<int> vec3 = sum_vecs<int>(vec1, vec2);
 	
 	// print the resulting vector
-	print_vec(vec3);
+	print_vec<int>(vec3);
 
 	return 0;
 }
